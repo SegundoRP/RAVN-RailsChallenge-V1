fff6b2a74a3167b0354a8fed989f41018dd048d0
segundo rebaza <alvaro.rp1593@gmail.com>
2022-03-14 23:20:00 -0500

    se instala sweet alert

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -2,11 +2,11 @@ class TrainersController < ApplicationController
   before_action :set_trainer, only: [:show, :edit, :update, :destroy]
 
   def index
-    @trainers = User.where("admin = false AND id != ?", current_user.id).paginate(page: params[:page], per_page: 3)
-
+    @trainers = policy_scope(User).where("admin = false AND id != ?", current_user.id).paginate(page: params[:page], per_page: 3)
+    @user = current_user
     if params[:search].present?
       if params[:search][:query].empty?
-        @trainers = User.where("admin = false AND id != ?", current_user.id).paginate(page: params[:page], per_page: 3)
+        @trainers = policy_scope(User).where("admin = false AND id != ?", current_user.id).paginate(page: params[:page], per_page: 3)
       else
         sql_query = " \
           first_name iLIKE :query \
@@ -14,13 +14,14 @@ class TrainersController < ApplicationController
           OR email iLIKE :query \
           OR CONCAT(first_name, ' ', last_name) iLIKE :query \
         "
-        @trainers = User.where(sql_query, query: "%#{params[:search][:query]}%").where("admin = false AND id != ?", current_user.id).paginate(page: params[:page], per_page: 3)
+        @trainers = policy_scope(User).where(sql_query, query: "%#{params[:search][:query]}%").where("admin = false AND id != ?", current_user.id).paginate(page: params[:page], per_page: 3)
       end
     end
   end
 
   def add
     @trainer = User.new
+    authorize @trainer
   end
 
   def show
@@ -29,6 +30,7 @@ class TrainersController < ApplicationController
 
   def create
     @trainer = User.new(user_params)
+    authorize @trainer
     @trainer.pokemons << Pokemon.find(params[:user][:pokemon_id].reject { |c| c.empty? })
     password_user = SecureRandom.uuid
     @trainer.username = "#{user_params[:first_name]}_#{user_params[:last_name]}"
@@ -46,6 +48,7 @@ class TrainersController < ApplicationController
   end
 
   def update
+    @trainer.pokemons << Pokemon.find(params[:user][:pokemons].reject { |c| c.empty? })
     if @trainer.update(user_params)
       redirect_to trainer_path(@trainer)
     else
@@ -62,6 +65,7 @@ class TrainersController < ApplicationController
 
   def set_trainer
     @trainer = User.find(params[:id])
+    authorize @trainer
   end
 
   def user_params
---
+++ app/policies/user_policy.rb
@@ -0,0 +1,33 @@
+class UserPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def add?
+    user_is_admin?
+  end
+
+  def create?
+    user_is_admin?
+  end
+
+  def show?
+    user_is_admin?
+  end
+
+  def update?
+    user_is_admin?
+  end
+
+  def destroy?
+    user_is_admin?
+  end
+
+  private
+
+  def user_is_admin?
+    user.admin
+  end
+end
